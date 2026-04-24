41d4585b86a61fc5bb0253a1613af11188194186
Muhammad Daffa Al Rizky <19250741@bsi.ac.id>
2026-04-24 15:36:09 +0700

    Latihan 3

--- index_navigasi.php
+++ index_navigasi.php
@@ -9,10 +9,44 @@
 
 <?php include 'navigasi.php'; ?>
 
-<div style="padding-left:16px">
-  <h2>Responsive Topnav Example</h2>
-  <p>Resize the browser window to see how it works.</p>
+<div style="padding-left:30px">
+  
+  <h2>Responsive Navigataion</h2>
+  <form method="post" action="" class ="mb-4">
+    <table style="width:92%; border-collapse: collapse;" border="1">
+      <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td><input type="text" name="nama" required></td>
+      </tr>
+  </form>
+  <table style="width:92%; border-collapse: collapse;" border="1">
+    <tr>
+      <td>NIM</td>
+      <td>:</td>
+      <td><input type="number" name="nim" required></td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin</td>
+      <td>:</td>
+      <td>
+        <input type="radio" name="jk" value="Laki-laki" required>Laki-laki
+        <input type="radio" name="jk" value="Perempuan" required>Perempuan
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align: right;">
+        <input type="submit" name="submit" value="Submit">
+        <?php if (isset($_POST['submit'])): ?>
+          <div style="margin-left: 20%;">
+            <strong>Hasil Submit:</strong><br>
+            <strong>NIM:</strong> <?php echo isset($_POST['nim']) ? $_POST['nim'] : 'NIM tidak diisi'; ?><br>
+            <strong>Nama:</strong> <?php echo isset($_POST['nama']) ? $_POST['nama'] : 'Nama tidak diisi'; ?><br>
+            <strong>Jenis Kelamin:</strong> <?php echo isset($_POST['jk']) ? $_POST['jk'] : 'Jenis Kelamin tidak diisi'; ?><br>
+          </div>
+        <?php endif; ?>
+    </tr>
+  </table>
 </div>
-
 </body>
 </html>
